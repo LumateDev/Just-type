3c7dfe5d339cf1359759902d404c6d8aa7a0fd9e
Alex <Sanchous557@mail.ru>
2023-09-19 00:31:05 +0300

    bug fix

--- src/components/InputText/data.js
+++ src/components/InputText/data.js
@@ -1,11 +1,10 @@
-export const letters = [
-  { id: 1, char: "l" },
-  { id: 2, char: "o" },
-  { id: 3, char: "r" },
-  { id: 4, char: " " },
-  { id: 5, char: "a" },
-  { id: 6, char: "b" },
-  { id: 7, char: "o" },
-  { id: 8, char: "b" },
-  { id: 9, char: "a" },
-];
+export const letters = [];
+let words = ["hello", "world", "js", "react", "aboba"];
+let str = words.join([" "]);
+
+for (let i = 0; i < str.length; i++) {
+  let obj = {};
+  obj.id = i + 1;
+  obj.char = str[i];
+  letters.push(obj);
+}
