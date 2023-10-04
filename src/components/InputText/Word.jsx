5f51fa47c74aa56e6e7a1b60a5b0d02e922fa004
Alex <Sanchous557@mail.ru>
2023-10-05 01:35:46 +0300

    links state with buttons and add type analyis

--- src/components/InputText/Word.jsx
+++ src/components/InputText/Word.jsx
@@ -2,27 +2,51 @@ import React from "react";
 import "./word.css";
 import Char from "./Char";
 
-const Word = React.forwardRef(({ word, i, len,  setActiveKey}, ref) => {
+const Word = React.forwardRef(
+  (
+    {
+      word,
+      i,
+      len,
+      setActiveKey,
+      setTotalTypedCharacters,
+      setTotalMistypedCharacters,
+      totalMistypedCharacters,
+      totalTypedCharacters,
+      startTime,
+      endTime,
+      setStartTime,
+      setEndTime,
+      setStatus,
+    },
+    ref
+  ) => {
     let w = word.split("");
 
-    if (i + 1 !== len){
-        w.push(" ");
-      }
-
-    
-   
+    if (i + 1 !== len) {
+      w.push(" ");
+    }
 
     const charItems = w.map((char, index) => (
-        <Char
-          key={index}
-          char={char}
-          word={word}
-          ref={ref}
-          setActiveKey={setActiveKey}
-          
-        ></Char>
-      ));
+      <Char
+        key={index}
+        char={char}
+        word={word}
+        ref={ref}
+        setActiveKey={setActiveKey}
+        totalTypedCharacters={totalTypedCharacters}
+        totalMistypedCharacters={totalMistypedCharacters}
+        setTotalTypedCharacters={setTotalTypedCharacters}
+        setTotalMistypedCharacters={setTotalMistypedCharacters}
+        setStartTime={setStartTime}
+        setEndTime={setEndTime}
+        startTime={startTime}
+        endTime={endTime}
+        setStatus={setStatus}
+      ></Char>
+    ));
     return <div className="word">{charItems}</div>;
-  });
-  
-  export default Word;
+  }
+);
+
+export default Word;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,23 +2,75 @@ import Header from "./../components/Header/Header";
 import InputText from "../components/InputText/InputText";
 import Toolbar from "../components/ToolBar/ToolBar";
 import Keyboard from "../components/Keyboard/Keyboard";
+import TestAnalysis from "../components/TestAnalyze/TestAnalysis";
 
 import { useState } from "react";
 
 const Home = () => {
+  const [status, setStatus] = useState("print");
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+  const [totalTypedCharacters, setTotalTypedCharacters] = useState(0);
+  const [totalMistypedCharacters, setTotalMistypedCharacters] = useState(0);
+  const [wordCount, setWordCount] = useState(25);
+  const [wordTime, setWordTime] = useState("15s");
+  const [restartTest, setRestartTest] = useState(0);
+  const [numbersInclude, setNumbersInclude] = useState(false);
+  const [punctuationInclude, setPunctuationInclude] = useState(false);
+  const [languageTest, setLanguageTest] = useState("english");
   const [activeKey, setActiveKey] = useState("");
+
   return (
     <>
-      {/* <!----------------------------HEADER----------------------------------------> */}
       <Header />
-      <InputText setActiveKey={setActiveKey} />
-      <Toolbar />
+      {status === "print" && (
+        <InputText
+          setStatus={setStatus}
+          startTime={startTime}
+          endTime={endTime}
+          setStartTime={setStartTime}
+          setEndTime={setEndTime}
+          setActiveKey={setActiveKey}
+          wordCount={wordCount}
+          wordTime={wordTime}
+          restartTest={restartTest}
+          numbersInclude={numbersInclude}
+          punctuationInclude={punctuationInclude}
+          languageTest={languageTest}
+          totalTypedCharacters={totalTypedCharacters}
+          totalMistypedCharacters={totalMistypedCharacters}
+          setTotalTypedCharacters={setTotalTypedCharacters}
+          setTotalMistypedCharacters={setTotalMistypedCharacters}
+        />
+      )}
+
+      {status === "analysis" && (
+        <TestAnalysis
+          wordCount={wordCount}
+          startTime={startTime}
+          endTime={endTime}
+          totalTypedCharacters={totalTypedCharacters}
+          totalMistypedCharacters={totalMistypedCharacters}
+        />
+      )}
+
+      <Toolbar
+        setStatus={setStatus}
+        activeCountButton={wordCount}
+        activeTimeButton={wordTime}
+        activeLanguageButton={languageTest}
+        activeNumbersButton={numbersInclude}
+        activePunctuationButton={punctuationInclude}
+        activeRestartButton={restartTest}
+        setActiveCountButton={setWordCount}
+        setActiveTimeButton={setWordTime}
+        setActiveLanguageButton={setLanguageTest}
+        setActiveNumbersButton={setNumbersInclude}
+        setActivePunctuationButton={setPunctuationInclude}
+        setActiveRestartButton={setRestartTest}
+      />
+
       <Keyboard activeKey={activeKey} />
-
-      {/* <!----------------------------HEADER----------------------------------------> */}
-      {/* <!-----------------------------MAIN-----------------------------------------> */}
-
-      {/* <!-----------------------------MAIN-----------------------------------------> */}
     </>
   );
 };
