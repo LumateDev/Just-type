9273bce27957cf868304ca96af5546d198e6b4e2
Alex <Sanchous557@mail.ru>
2023-11-21 02:57:47 +0300

    add base style form and base validation

--- src/components/RegAndLogForm/RegAndLogForm.jsx
+++ src/components/RegAndLogForm/RegAndLogForm.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Modal from "react-modal";
 import "./regAndLogForm.css";
+import { useForm } from "react-hook-form";
+import errorSvg from "../../img/form/form-error.svg";
 
 const RegAndLogForm = ({
   modalOpen,
@@ -8,17 +10,42 @@ const RegAndLogForm = ({
   formTitle,
   setFormTitle,
 }) => {
-  const handleRegister = () => {
-    alert("Запрос на " + formTitle + " принят");
-  };
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    mode: "onBlur",
+  });
 
-  const handleLogin = () => {
-    alert("Запрос на " + formTitle + " принят");
-  };
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
 
-  const handleRecover = () => {
-    alert("Запрос на " + formTitle + " принят");
+    reset();
+
+    switch (formTitle) {
+      case "Регистрация":
+        handleRegister();
+        break;
+      case "Восстановление":
+        handleRecover();
+        break;
+      default:
+        handleLogin();
+    }
   };
+  function handleRegister() {
+    console.log("Запуск функции handleRegister");
+  }
+
+  function handleRecover() {
+    console.log("Запуск функции handleRecover");
+  }
+
+  function handleLogin() {
+    console.log("Запуск функции handleLogin");
+  }
 
   Modal.setAppElement("#root");
 
@@ -29,34 +56,120 @@ const RegAndLogForm = ({
       className="modal-form"
       overlayClassName="overlay"
     >
-      <form className="form-wrapper">
+      <form className="form-wrapper" onSubmit={handleSubmit(onSubmit)}>
         <div className="form-title-wrapper">
           <h2 className="form-title">{formTitle}</h2>
         </div>
 
-        <div className="form-row">
-          <input type="text" autoComplete="username" placeholder="Username" />
+        <div className="form-col">
+          <div className="form-input">
+            {errors?.username && (
+              <img src={errorSvg} width="32px" height="32px" alt="error icon" />
+            )}
+            <input
+              {...register("username", {
+                required: "Поле обязательно для заполнения",
+                minLength: {
+                  value: 5,
+                  message: "Длина поля не может быть меньше 5 символов",
+                },
+              })}
+              type="text"
+              autoComplete="username"
+              placeholder="Username"
+            />
+          </div>
+
+          <div className="username-error">
+            {errors?.username && <p>{errors?.username?.message || "Error!"}</p>}
+          </div>
         </div>
+
         {formTitle === "Регистрация" && (
-          <div className="form-row">
-            <input type="email" autoComplete="email" placeholder="Email" />
+          <div className="form-col">
+            <div className="form-input">
+              {errors?.email && (
+                <img
+                  src={errorSvg}
+                  width="32px"
+                  height="32px"
+                  alt="error icon"
+                />
+              )}
+              <input
+                {...register("email", {
+                  required: "Поле обязательно для заполнения",
+                  minLength: {
+                    value: 8,
+                    message: "Длина поля не может быть меньше 8 символов",
+                  },
+                })}
+                type="email"
+                autoComplete="email"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="email-error">
+              {errors?.email && <p>{errors?.email?.message || "Error!"}</p>}
+            </div>
           </div>
         )}
 
-        <div className="form-row">
-          <input
-            type="password"
-            autoComplete="current-password"
-            placeholder="Password"
-          />
-        </div>
-        {formTitle === "Регистрация" && (
-          <div className="form-row">
+        <div className="form-col">
+          <div className="form-input">
+            {errors?.password && (
+              <img src={errorSvg} width="32px" height="32px" alt="error icon" />
+            )}
             <input
+              {...register("password", {
+                required: "Поле обязательно для заполнения",
+                minLength: {
+                  value: 8,
+                  message: "Длина пароля не может быть меньше 5 символов",
+                },
+              })}
               type="password"
               autoComplete="current-password"
-              placeholder="Repeat password"
+              placeholder="Password"
             />
+          </div>
+
+          <div className="password-error">
+            {errors?.password && <p>{errors?.password?.message || "Error!"}</p>}
+          </div>
+        </div>
+
+        {formTitle === "Регистрация" && (
+          <div className="form-col">
+            <div className="form-input">
+              {errors?.repeatPassword && (
+                <img
+                  src={errorSvg}
+                  width="32px"
+                  height="32px"
+                  alt="error icon"
+                />
+              )}
+              <input
+                {...register("repeatPassword", {
+                  required: "Поле обязательно для заполнения",
+                  minLength: {
+                    value: 8,
+                    message: "Длина пароля не может быть меньше 5 символов",
+                  },
+                })}
+                type="password"
+                autoComplete="current-password"
+                placeholder="Repeat Password"
+              />
+            </div>
+
+            <div className="repeat-password-error">
+              {errors?.repeatPassword && (
+                <p>{errors?.repeatPassword?.message || "Error!"}</p>
+              )}
+            </div>
           </div>
         )}
 
@@ -65,6 +178,7 @@ const RegAndLogForm = ({
             <button
               onClick={(e) => {
                 e.preventDefault();
+                reset();
                 formTitle === "Регистрация"
                   ? setFormTitle("Авторизация")
                   : setFormTitle("Восстановление");
@@ -78,17 +192,7 @@ const RegAndLogForm = ({
         </div>
 
         <div className="form-btn">
-          <button
-            type="submit"
-            onClick={(e) => {
-              e.preventDefault();
-              formTitle === "Регистрация"
-                ? handleRegister()
-                : formTitle === "Восстановление"
-                ? handleRecover()
-                : handleLogin();
-            }}
-          >
+          <button type="submit" disabled={!isValid}>
             {formTitle === "Регистрация"
               ? "Создать аккаунт"
               : formTitle === "Восстановление"
